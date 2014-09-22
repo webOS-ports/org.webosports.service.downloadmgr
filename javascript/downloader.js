6cec1b457e1dd7d6990cca55c5fb25c19733b497
Achim Königs <garfonso@mobo.info>
2014-09-22 17:59:40 +0200

    first sceleton
    Signed-off-by: Achim Königs <garfonso@mobo.info>

---
+++ javascript/downloader.js
@@ -0,0 +1,24 @@
+var fs = require("fs"),
+    http = require("http"),
+    https = require("https"),
+    url = require("url"),
+    Future = require("./Future");
+
+var Downloader = function () {
+    "use strict";
+    //private variables:
+
+
+    //private functions:
+
+
+    //public interface:
+    return {
+        download: function () {
+
+
+        }
+    };
+};
+
+module.exports = Downloader;
