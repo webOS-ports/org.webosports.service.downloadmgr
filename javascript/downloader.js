d16980db038bfa12986acec1634285f236f02eb8
Achim Königs <garfonso@mobo.info>
2015-07-06 11:28:30 +0200

    whitespace changes
    Signed-off-by: Achim Königs <garfonso@mobo.info>

--- javascript/downloader.js
+++ javascript/downloader.js
@@ -1,24 +1,18 @@
-var fs = require("fs"),
-    http = require("http"),
-    https = require("https"),
-    url = require("url"),
-    Future = require("./Future");
+/*jslint node: true */
 
 var Downloader = function () {
-    "use strict";
-    //private variables:
+	"use strict";
+	//private variables:
+
+	//private functions:
 
 
-    //private functions:
+	//public interface:
+	return {
+		download: function (options) {
 
-
-    //public interface:
-    return {
-        download: function () {
-
-
-        }
-    };
+		}
+	};
 };
 
 module.exports = Downloader;
